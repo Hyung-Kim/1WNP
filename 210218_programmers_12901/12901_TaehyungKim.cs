550ca8ec5235acc15a05dee6d11e677ce145395b
Taehuyng Kim <xodaktmdnj@naver.com>
2021-02-24 10:54:38 +0900

    #5 12901 solved

---
+++ 210218_programmers_12901/12901_TaehyungKim.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Programmers.Study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var solution = new Solution();
+            var ret = solution.solution(1, 2);
+            Debug.WriteLine(ret);
+                
+        }
+    }
+    public class Solution
+    {
+        public string solution(int a, int b) =>
+            new DateTime(2016, a, b).DayOfWeek.ToString().Substring(0, 3).ToUpper();
+    }
+}
