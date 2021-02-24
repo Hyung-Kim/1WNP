69fd0acfd0212875f5b88b0d0fac5e0a587b1ac4
Taehuyng Kim <xodaktmdnj@naver.com>
2021-02-24 13:04:12 +0900

    #5 solved 12903

---
+++ 210218_programmers_12903/12903_TaehyungKim.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Programmers.Study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            var ret = solution.solution("qwer");
+        }
+    }
+
+
+
+    public class Solution
+    {
+        public string solution(string s)
+        {
+
+            var length = s.Length;
+            var isEven = (length % 2) == 0;
+            var stx = length / 2;
+            if (isEven)
+            {
+                return string.Concat(s.Skip(stx - 1).Take(2));
+            }
+            else
+            {
+                return string.Concat(s.Skip(stx).Take(1));
+            }
+        }
+    }
+}
